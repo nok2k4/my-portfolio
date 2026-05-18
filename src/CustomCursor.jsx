6c266f7d3877d37bf88402a466729a1bf2bb5ab8
chjl04 <ngocqv.it@gmail.com>
2026-05-18 21:59:02 +0700

    sua tro chuot

--- src/CustomCursor.jsx
+++ src/CustomCursor.jsx
@@ -89,11 +89,7 @@ const CustomCursor = () => {
     <>
       <div ref={glowRef} className="glow-cursor"></div>
       <div ref={outlineRef} className={`cursor-outline ${isHovering ? 'hover' : ''}`}></div>
-      <div ref={dotRef} className={`cursor-dot ${isHovering ? 'hover' : ''}`}>
-        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="var(--accent-color)" stroke="white" strokeWidth="1" strokeLinecap="round" strokeLinejoin="round" style={{ transform: 'rotate(-20deg) scale(0.8)', marginLeft: '10px', marginTop: '10px' }}>
-          <path d="M3 3l7.07 16.97 2.51-7.39 7.39-2.51L3 3z"></path>
-        </svg>
-      </div>
+      <div ref={dotRef} className={`cursor-dot ${isHovering ? 'hover' : ''}`}></div>
     </>
   );
 };
